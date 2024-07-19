be38f3992af3ce2c3278de07c5d4db57d3656992
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-19 16:30:55 -0600

    DTO - Data transfer object validate what is sent to the database

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from './entities/auth.entity';
+import { User, UserSchema } from './entities/user.entity';
 
 @Module({
   controllers: [AuthController],
