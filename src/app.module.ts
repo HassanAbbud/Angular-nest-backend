1a40c2bf7d0157949f345aee145b966e1190dbff
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-19 14:21:31 -0600

    Environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot('mongodb://localhost/27017'),
 
     AuthModule, 
